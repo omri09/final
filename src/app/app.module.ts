98296837ef27e10d46ecc970aa20fdb7939b4fd5
omri09 <omri620@gmail.com>
2020-03-22 13:52:24 +0200

    try

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+
+
+
+@NgModule({
+  
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+ 
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+
+  
+})
+export class AppModule { 
+
+}
