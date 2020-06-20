329c9052c5594fc1a23d2f95ac311d0098b16d35
omri09 <omri620@gmail.com>
2020-06-21 00:50:17 +0300

    c

--- routes.js
+++ routes.js
@@ -6,9 +6,9 @@ const studentRoute = express.Router();
 // add the student model
 let Student = require('./schema');
 
-studentRoute.use(express.static('../../dist/Project'));
+studentRoute.use(express.static(__dirname+'/dist/Project'));
 studentRoute.get('/show-list', function (req, res) {
-    res.sendFile('../../dist/Project/index.html');
+    res.sendFile(__dirname+'/dist/Project/index.html');
 });
 
 /// Get list of students
