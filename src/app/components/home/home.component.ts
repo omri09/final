909ff141a6225b4b74d9d1e11e61915d5429b671
omri09 <omri620@gmail.com>
2020-08-29 15:15:59 +0300

    fgf

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  
   showNavigationArrows = true;
   showNavigationIndicators = true;
   images = [1055, 194, 368].map((n) => `https://picsum.photos/id/${n}/900/500`);
